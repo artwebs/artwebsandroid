d2f12e487e5566b575ac407c0191e477d091d73d
www <your_email@youremail.com>
2012-09-24 10:17:14 +0800

    导入

---
+++ artwebsandroid/src/com/artwebs/ArtwebsandroidActivity.java
@@ -0,0 +1,13 @@
+package com.artwebs;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ArtwebsandroidActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
