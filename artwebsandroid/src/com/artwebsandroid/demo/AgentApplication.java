74bc7556c6ffe6ba62d16c2d6db6faeefb9677b0
www <your_email@youremail.com>
2012-10-09 09:21:14 +0800

    添加AgentApplication.java及C.java

---
+++ artwebsandroid/src/com/artwebsandroid/demo/AgentApplication.java
@@ -0,0 +1,43 @@
+package com.artwebsandroid.demo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.app.Activity;
+import android.app.Application;
+
+public class AgentApplication extends Application {
+	private List<Activity> activities = new ArrayList<Activity>();  
+	private String loginName="";
+	private String password="";
+	public static String loginKey="";
+    public String getLoginName() {
+		return loginName;
+	}
+
+	public void setLoginName(String loginName) {
+		this.loginName = loginName;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public void addActivity(Activity activity) {  
+        activities.add(activity);  
+    }  
+  
+    @Override  
+    public void onTerminate() {  
+        super.onTerminate();  
+          
+        for (Activity activity : activities) {  
+            activity.finish();  
+        }  
+        System.exit(0);  
+    }
+}
---
+++ artwebsandroid/src/com/artwebsandroid/demo/C.java
@@ -0,0 +1,10 @@
+package com.artwebsandroid.demo;
+
+public final class C {
+	public final static class transmit	{
+		public final static String host="http://192.168.1.18:8100/";
+		public final static String skip="hribs/index.php?";
+		public static String addCmd="&key="+AgentApplication.loginKey;
+	}
+	
+}
