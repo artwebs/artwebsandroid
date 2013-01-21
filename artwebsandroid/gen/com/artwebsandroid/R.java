128c13531eb841d882c4185bd20d265bdb4c7b59
artwebs <artwebs@126.com>
2013-01-21 14:37:54 +0800

    update

--- artwebsandroid/gen/com/artwebsandroid/R.java
+++ artwebsandroid/gen/com/artwebsandroid/R.java
@@ -11,35 +11,35 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int ListViewPeople=0x7f070001;
-        public static final int findkey=0x7f070004;
-        public static final int first=0x7f070002;
-        public static final int installApkButton=0x7f070007;
-        public static final int listButton=0x7f070006;
-        public static final int menu_settings=0x7f070009;
-        public static final int mytextxx=0x7f070000;
-        public static final int second=0x7f070003;
-        public static final int text1=0x7f070005;
-        public static final int uiButton=0x7f070008;
+        public static int ListViewPeople=0x7f070001;
+        public static int findkey=0x7f070004;
+        public static int first=0x7f070002;
+        public static int installApkButton=0x7f070007;
+        public static int listButton=0x7f070006;
+        public static int menu_settings=0x7f070009;
+        public static int mytextxx=0x7f070000;
+        public static int second=0x7f070003;
+        public static int text1=0x7f070005;
+        public static int uiButton=0x7f070008;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
-        public static final int binlist=0x7f030001;
-        public static final int binlistitem=0x7f030002;
-        public static final int diclistitem=0x7f030003;
-        public static final int main=0x7f030004;
+        public static int activity_main=0x7f030000;
+        public static int binlist=0x7f030001;
+        public static int binlistitem=0x7f030002;
+        public static int diclistitem=0x7f030003;
+        public static int main=0x7f030004;
     }
     public static final class menu {
-        public static final int activity_main=0x7f060000;
+        public static int activity_main=0x7f060000;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int hello=0x7f040003;
-        public static final int hello_world=0x7f040001;
-        public static final int menu_settings=0x7f040002;
+        public static int app_name=0x7f040000;
+        public static int hello=0x7f040003;
+        public static int hello_world=0x7f040001;
+        public static int menu_settings=0x7f040002;
     }
     public static final class style {
         /** 
@@ -52,10 +52,10 @@ public final class R {
             backward-compatibility can go here.
         
          */
-        public static final int AppBaseTheme=0x7f050000;
+        public static int AppBaseTheme=0x7f050000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f050001;
+        public static int AppTheme=0x7f050001;
     }
 }
