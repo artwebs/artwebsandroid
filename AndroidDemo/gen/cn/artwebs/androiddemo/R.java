b97018638d343d2e16082707f046170dc736586c
artwebs <artwebs@126.com>
2014-03-11 16:06:06 +0800

    增加默认图片和主菜单控件功能

--- AndroidDemo/gen/cn/artwebs/androiddemo/R.java
+++ AndroidDemo/gen/cn/artwebs/androiddemo/R.java
@@ -22,48 +22,53 @@ public final class R {
     }
     public static final class drawable {
         public static final int datetimeicon=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
-        public static final int img_progress=0x7f020002;
-        public static final int mic=0x7f020003;
-        public static final int progress=0x7f020004;
+        public static final int default_bg=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int img_progress=0x7f020003;
+        public static final int menu_default_icon=0x7f020004;
+        public static final int menu_default_icon_bg=0x7f020005;
+        public static final int mic=0x7f020006;
+        public static final int progress=0x7f020007;
     }
     public static final class id {
-        public static final int ListViewPeople=0x7f080004;
-        public static final int action_settings=0x7f080016;
-        public static final int artcamera=0x7f080009;
-        public static final int artqrcode=0x7f080014;
-        public static final int datepicker=0x7f08000d;
-        public static final int findkey=0x7f080007;
-        public static final int first=0x7f080005;
-        public static final int imageView=0x7f080010;
+        public static final int ListViewPeople=0x7f080005;
+        public static final int action_settings=0x7f080017;
+        public static final int artcamera=0x7f08000a;
+        public static final int artqrcode=0x7f080015;
+        public static final int datepicker=0x7f08000e;
+        public static final int findkey=0x7f080008;
+        public static final int first=0x7f080006;
+        public static final int imageView=0x7f080011;
         public static final int image_view=0x7f080001;
-        public static final int installApkButton=0x7f080012;
-        public static final int listButton=0x7f080011;
-        public static final int megTxt=0x7f080008;
-        public static final int menu_settings=0x7f080015;
-        public static final int mytextxx=0x7f080003;
-        public static final int saveAudioBtn=0x7f08000c;
-        public static final int saveImageBtn=0x7f08000a;
-        public static final int saveVideoBtn=0x7f08000b;
-        public static final int second=0x7f080006;
-        public static final int text1=0x7f08000f;
-        public static final int timepicker=0x7f08000e;
-        public static final int uiButton=0x7f080013;
+        public static final int installApkButton=0x7f080013;
+        public static final int listButton=0x7f080012;
+        public static final int megTxt=0x7f080009;
+        public static final int menuTbl=0x7f080003;
+        public static final int menu_settings=0x7f080016;
+        public static final int mytextxx=0x7f080004;
+        public static final int saveAudioBtn=0x7f08000d;
+        public static final int saveImageBtn=0x7f08000b;
+        public static final int saveVideoBtn=0x7f08000c;
+        public static final int second=0x7f080007;
+        public static final int text1=0x7f080010;
+        public static final int timepicker=0x7f08000f;
+        public static final int uiButton=0x7f080014;
         public static final int video_timer=0x7f080002;
         public static final int video_view=0x7f080000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int artcamera=0x7f030001;
-        public static final int binlist=0x7f030002;
-        public static final int binlistitem=0x7f030003;
-        public static final int camera=0x7f030004;
-        public static final int datetime=0x7f030005;
-        public static final int diclistitem=0x7f030006;
-        public static final int imgbinlistitem=0x7f030007;
-        public static final int layout_progress=0x7f030008;
-        public static final int main=0x7f030009;
-        public static final int qrcode=0x7f03000a;
+        public static final int artmenu_layout=0x7f030002;
+        public static final int binlist=0x7f030003;
+        public static final int binlistitem=0x7f030004;
+        public static final int camera=0x7f030005;
+        public static final int datetime=0x7f030006;
+        public static final int diclistitem=0x7f030007;
+        public static final int imgbinlistitem=0x7f030008;
+        public static final int layout_progress=0x7f030009;
+        public static final int main=0x7f03000a;
+        public static final int qrcode=0x7f03000b;
     }
     public static final class menu {
         public static final int activity_main=0x7f070000;
--- artwebsandroid/gen/cn/artwebs/R.java
+++ artwebsandroid/gen/cn/artwebs/R.java
@@ -12,39 +12,44 @@ public final class R {
     }
     public static final class drawable {
         public static int datetimeicon=0x7f020000;
-        public static int ic_launcher=0x7f020001;
-        public static int img_progress=0x7f020002;
-        public static int mic=0x7f020003;
-        public static int progress=0x7f020004;
+        public static int default_bg=0x7f020001;
+        public static int ic_launcher=0x7f020002;
+        public static int img_progress=0x7f020003;
+        public static int menu_default_icon=0x7f020004;
+        public static int menu_default_icon_bg=0x7f020005;
+        public static int mic=0x7f020006;
+        public static int progress=0x7f020007;
     }
     public static final class id {
-        public static int ListViewPeople=0x7f070004;
-        public static int datepicker=0x7f070008;
-        public static int findkey=0x7f070007;
-        public static int first=0x7f070005;
-        public static int imageView=0x7f07000b;
+        public static int ListViewPeople=0x7f070005;
+        public static int datepicker=0x7f070009;
+        public static int findkey=0x7f070008;
+        public static int first=0x7f070006;
+        public static int imageView=0x7f07000c;
         public static int image_view=0x7f070001;
-        public static int installApkButton=0x7f07000d;
-        public static int listButton=0x7f07000c;
-        public static int menu_settings=0x7f07000f;
-        public static int mytextxx=0x7f070003;
-        public static int second=0x7f070006;
-        public static int text1=0x7f07000a;
-        public static int timepicker=0x7f070009;
-        public static int uiButton=0x7f07000e;
+        public static int installApkButton=0x7f07000e;
+        public static int listButton=0x7f07000d;
+        public static int menuTbl=0x7f070003;
+        public static int menu_settings=0x7f070010;
+        public static int mytextxx=0x7f070004;
+        public static int second=0x7f070007;
+        public static int text1=0x7f07000b;
+        public static int timepicker=0x7f07000a;
+        public static int uiButton=0x7f07000f;
         public static int video_timer=0x7f070002;
         public static int video_view=0x7f070000;
     }
     public static final class layout {
         public static int activity_main=0x7f030000;
         public static int artcamera=0x7f030001;
-        public static int binlist=0x7f030002;
-        public static int binlistitem=0x7f030003;
-        public static int datetime=0x7f030004;
-        public static int diclistitem=0x7f030005;
-        public static int imgbinlistitem=0x7f030006;
-        public static int layout_progress=0x7f030007;
-        public static int main=0x7f030008;
+        public static int artmenu_layout=0x7f030002;
+        public static int binlist=0x7f030003;
+        public static int binlistitem=0x7f030004;
+        public static int datetime=0x7f030005;
+        public static int diclistitem=0x7f030006;
+        public static int imgbinlistitem=0x7f030007;
+        public static int layout_progress=0x7f030008;
+        public static int main=0x7f030009;
     }
     public static final class menu {
         public static int activity_main=0x7f060000;
