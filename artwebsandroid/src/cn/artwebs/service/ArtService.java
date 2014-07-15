eb4ef456297478941415adbc2e22ea679d570366
artwebs <artwebs@126.com>
2014-07-15 17:57:19 +0800

    修改通用服务类

--- artwebsandroid/src/cn/artwebs/service/ArtService.java
+++ artwebsandroid/src/cn/artwebs/service/ArtService.java
@@ -10,17 +10,32 @@ import android.os.IBinder;
 import android.support.v4.content.LocalBroadcastManager;
 
 public abstract class ArtService extends Service {
-	private static Activity activity;
-	private static Intent serviceIntent;
-	private static AlarmManager manager;
-	private static PendingIntent pendingIntent;
+	
 	private static LocalBroadcastManager broadcaster;
 	
 	private static String SERVICE_RESULT="cn.artwebs.service.ArtService";
 	public final static String RESULT_TAG="tag";
 	private static boolean isSendbroad=false;
-	private static long intervalTime=15*1000;
 	private static ArtService self;
+	
+	
+	
+	public static void setServiceResult(String tag)
+	{
+		SERVICE_RESULT=tag;
+		if(SERVICE_RESULT==null)isSendbroad=false;
+		
+	}
+	
+	public static void setBroadcaster(LocalBroadcastManager bdcaster)
+	{
+		broadcaster=bdcaster;
+	}
+	
+	public  static String getserviceSign()
+	{
+		return SERVICE_RESULT;
+	}
 	
 	@Override
 	public IBinder onBind(Intent intent) {
@@ -28,52 +43,7 @@ public abstract class ArtService extends Service {
 		return null;
 	}
 	
-	public static void start(Activity obj)
-	{
-		start(obj,0,null);
-	}
 	
-	public static void start(Activity obj,long iTime)
-	{
-		start(obj,iTime,null);
-	}
-	
-	public static void start(Activity obj,long iTime,String serviceSign)
-	{
-		if(activity==null)
-		{
-			activity=obj;
-			SERVICE_RESULT=serviceSign;
-			if(iTime!=0)intervalTime=iTime;
-			if(SERVICE_RESULT==null)isSendbroad=false;
-			broadcaster=LocalBroadcastManager.getInstance(activity);
-			serviceIntent=new Intent(obj,new Object(){
-				public Class getToClass()
-				{
-					return this.getClass();
-				}
-			}.getToClass());
-    		if(manager==null)manager=(AlarmManager) activity.getSystemService(Context.ALARM_SERVICE);
-     		if(pendingIntent==null)pendingIntent=PendingIntent.getService(activity, 0, serviceIntent, 0);
-     		manager.setRepeating(AlarmManager.RTC, 0, intervalTime,pendingIntent);
-    		
-		}
-	}
-	
-	public static void stop()
-	{
-		if(activity!=null)
-		{
-			manager.cancel(pendingIntent);
-			activity.stopService(serviceIntent);
-			activity=null;
-		}
-	}
-	
-	public  static String getserviceSign()
-	{
-		return SERVICE_RESULT;
-	}
 	
 	
 	@Override
