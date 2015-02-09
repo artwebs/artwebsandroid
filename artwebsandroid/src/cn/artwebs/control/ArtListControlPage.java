0f6e7aa53f31944ec7b35bc9fe0ab4bb3abb21ed
rsmac <rsmac@ruanshu.com>
2015-02-09 17:53:53 +0800

    优化分页控制功能

--- artwebsandroid/src/cn/artwebs/control/ArtListControlPage.java
+++ artwebsandroid/src/cn/artwebs/control/ArtListControlPage.java
@@ -10,16 +10,16 @@ import cn.artwebs.object.BinList;
 
 public class ArtListControlPage implements OnScrollListener {
 	private final static String tag="ArtListViewPage";
-	private BinList list=new BinList();
+	protected BinList list=new BinList();
 	protected  Activity window;
-	private ListAdapter adapter;
+	protected ListAdapter adapter;
 	protected int page=1;
 	protected int pageSize=10;
-	private int visibleLastIndex = 0;  
-	private int visibleItemCount=0;  
-	private int dataSize=0;
+	protected int visibleLastIndex = 0;
+	protected int visibleItemCount=0;
+	protected int dataSize=0;
 	protected OnControlPageListener listener;
-	
+
 	public BinList getList() {
 		return list;
 	}
@@ -63,7 +63,7 @@ public class ArtListControlPage implements OnScrollListener {
 			int visibleItemCount, int totalItemCount) {
 		this.visibleItemCount = visibleItemCount;  
         visibleLastIndex = firstVisibleItem + visibleItemCount;    
-        if(totalItemCount == list.size()+1){  
+        if(totalItemCount == list.size()+1){
 //	            listView.removeFooterView(loadMoreLayout);
             Toast.makeText(window, "加载完成", Toast.LENGTH_LONG).show();  
         }  
@@ -71,21 +71,21 @@ public class ArtListControlPage implements OnScrollListener {
 
 	@Override
 	public void onScrollStateChanged(AbsListView view, int scrollState) {
-		Log.d(tag, "onScrollStateChanged");
+		Log.e(tag, "onScrollStateChanged");
 		int itemsLastIndex = adapter.getCount()-1;    
         int lastIndex = itemsLastIndex + 1;  
-        Log.d(tag, (scrollState == OnScrollListener.SCROLL_STATE_TOUCH_SCROLL )+"");
-        Log.d(tag, (visibleLastIndex == lastIndex )+"");
-        Log.d(tag, (adapter.getCount()<dataSize-1)+"");
-        Log.d(tag, "------------------------");
+        Log.e(tag, (scrollState == OnScrollListener.SCROLL_STATE_TOUCH_SCROLL) + "");
+        Log.e(tag, (visibleLastIndex == lastIndex) + "");
+        Log.e(tag, (adapter.getCount() < dataSize - 1) + "");
+        Log.e(tag, "------------------------");
         if (scrollState == OnScrollListener.SCROLL_STATE_TOUCH_SCROLL  
                 && visibleLastIndex == lastIndex &&adapter.getCount()<dataSize-1) { 
-        	Log.d(tag, "loadMoreData");
+        	Log.e(tag, "loadMoreData");
         	loadData();
         }  
 	}
 	
-	private void loadData()
+	protected void loadData()
 	{
 		if(listener==null)
         {
--- artwebsandroid/src/cn/artwebs/control/ArtListControlViewPage.java
+++ artwebsandroid/src/cn/artwebs/control/ArtListControlViewPage.java
@@ -1,27 +1,69 @@
 package cn.artwebs.control;
 
 import android.app.Activity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
 import android.widget.*;
 import cn.artwebs.ListAdapter.ListAdapter;
 import cn.artwebs.R;
 import cn.artwebs.comm.AppApplication;
 
 public class ArtListControlViewPage extends ArtListControlPage {
-	private View headerLayout;
-	private View footerLayout;
+	private final String tag="ArtListControlViewPage";
+	private final RotateAnimation animation;
+	private final ImageView arrowImageView;
+	private View headerView;
+	private View footerView;
 	private LayoutInflater inflater;
+	private ListView listView;
+	private boolean isLastRow = false;
+	private int lastPage =0;
 
 	public ArtListControlViewPage(Activity window,ListAdapter adapter,ListView listView){
 		super(window,adapter);
+		this.listView=listView;
 		inflater=LayoutInflater.from(AppApplication.getAppContext());
-		footerLayout = (View) inflater.inflate(R.layout.artlistview_footer,null);
-		headerLayout= (View) inflater.inflate(R.layout.artlistview_head,null);
-		listView.addHeaderView(headerLayout);
-		listView.addFooterView(footerLayout);
+		footerView = (View) inflater.inflate(R.layout.artlistview_footer,null);
+		headerView = (View) inflater.inflate(R.layout.artlistview_head,null);
+//		listView.addHeaderView(headerView);
+		listView.addFooterView(footerView);
 		listView.setAdapter(adapter);
 		listView.setOnScrollListener(this);
+
+		arrowImageView = (ImageView) headerView
+				.findViewById(R.id.head_arrowImageView);
+
+		animation = new RotateAnimation(0, -180,
+				RotateAnimation.RELATIVE_TO_SELF, 0.5f,
+				RotateAnimation.RELATIVE_TO_SELF, 0.5f);
+		animation.setInterpolator(new LinearInterpolator());
+		animation.setDuration(250);
+		animation.setFillAfter(true);
+
+//		arrowImageView.setAnimation(animation);
+	}
+
+
+	@Override
+	public void onScroll(AbsListView view, int firstVisibleItem,
+						 int visibleItemCount, int totalItemCount) {
+		if (!isLastRow&&firstVisibleItem + visibleItemCount == totalItemCount && totalItemCount > 0 ) {
+			isLastRow = true;
+		}
+	}
+
+	@Override
+	public void onScrollStateChanged(AbsListView view, int scrollState) {
+		Log.e(tag, "onScrollStateChanged");
+		if (lastPage !=page&&isLastRow && scrollState == AbsListView.OnScrollListener.SCROLL_STATE_IDLE) {
+			//加载元素
+			loadData();
+			isLastRow = false;
+			lastPage =page;
+		}
 	}
 
 
