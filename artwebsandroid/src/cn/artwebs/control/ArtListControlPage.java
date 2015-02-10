b228d74bfa2d601d9645669eefee18405c1654e0
rsmac <rsmac@ruanshu.com>
2015-02-10 14:45:05 +0800

    优化分页控制功能

--- artwebsandroid/src/cn/artwebs/control/ArtListControlPage.java
+++ artwebsandroid/src/cn/artwebs/control/ArtListControlPage.java
@@ -10,19 +10,18 @@ import cn.artwebs.object.BinList;
 
 public class ArtListControlPage implements OnScrollListener {
 	private final static String tag="ArtListViewPage";
-	protected BinList list=new BinList();
 	protected  Activity window;
+	protected  BinList list=new BinList();
 	protected ListAdapter adapter;
 	protected int page=1;
 	protected int pageSize=10;
-	protected int visibleLastIndex = 0;
-	protected int visibleItemCount=0;
-	protected int dataSize=0;
+	protected int dataSize=-1;
+
+	protected boolean isLastRow = false;
+	protected int lastPage =0;
+
 	protected OnControlPageListener listener;
 
-	public BinList getList() {
-		return list;
-	}
 
 	public void setDataSize(int dataSize) {
 		this.dataSize = dataSize;
@@ -46,8 +45,6 @@ public class ArtListControlPage implements OnScrollListener {
 		this.adapter.clearItem();
 		page=1;
 		setDataSize(pageSize);
-		visibleLastIndex = 0;
-		visibleItemCount=0;
 		list.clear();
 		loadData();
 	}
@@ -61,53 +58,56 @@ public class ArtListControlPage implements OnScrollListener {
 	@Override
 	public void onScroll(AbsListView view, int firstVisibleItem,
 			int visibleItemCount, int totalItemCount) {
-		this.visibleItemCount = visibleItemCount;  
-        visibleLastIndex = firstVisibleItem + visibleItemCount;    
-        if(totalItemCount == list.size()+1){
-//	            listView.removeFooterView(loadMoreLayout);
-            Toast.makeText(window, "加载完成", Toast.LENGTH_LONG).show();  
-        }  
+
+
+		if (!isLastRow&&firstVisibleItem + visibleItemCount == totalItemCount && totalItemCount > 0 ) {
+			isLastRow = true;
+		}
+
+		if(dataSize!=-1&&dataSize<=totalItemCount){
+			finishLoadData();
+			isLastRow=false;
+		}
+
+	}
+
+	protected void finishLoadData(){
+		Toast.makeText(window, "加载完成", Toast.LENGTH_LONG).show();
 	}
 
 	@Override
 	public void onScrollStateChanged(AbsListView view, int scrollState) {
 		Log.e(tag, "onScrollStateChanged");
-		int itemsLastIndex = adapter.getCount()-1;    
-        int lastIndex = itemsLastIndex + 1;  
-        Log.e(tag, (scrollState == OnScrollListener.SCROLL_STATE_TOUCH_SCROLL) + "");
-        Log.e(tag, (visibleLastIndex == lastIndex) + "");
-        Log.e(tag, (adapter.getCount() < dataSize - 1) + "");
-        Log.e(tag, "------------------------");
-        if (scrollState == OnScrollListener.SCROLL_STATE_TOUCH_SCROLL  
-                && visibleLastIndex == lastIndex &&adapter.getCount()<dataSize-1) { 
-        	Log.e(tag, "loadMoreData");
-        	loadData();
-        }  
+		if (lastPage !=page&&isLastRow && scrollState == AbsListView.OnScrollListener.SCROLL_STATE_IDLE) {
+			//加载元素
+			loadData();
+			isLastRow = false;
+			lastPage =page;
+		}
 	}
 	
 	protected void loadData()
 	{
 		if(listener==null)
         {
-			setDataSize(0);
+			setDataSize(-1);
         	Log.d(tag, "listener 没有设置");
         	return;
         }
 		listener.loadMoreData(page,pageSize);
 	}
 	
-	public void notifyDataChanged(BinList tmpList)
+	public synchronized void notifyDataChanged(BinList tmpList)
 	{
 		if(tmpList.size()>0)
     	{
-    		list.addend(tmpList);
+			list.addend(tmpList);
     		window.runOnUiThread(new Runnable() {
 				@Override
-				public void run() {
+				public synchronized void run() {
 					adapter.notifyDataSetChanged();
 				}
 			});
-    		dataSize+=tmpList.size();
     		page++;
     	}
 	}
--- artwebsandroid/src/cn/artwebs/control/ArtListControlViewPage.java
+++ artwebsandroid/src/cn/artwebs/control/ArtListControlViewPage.java
@@ -19,8 +19,7 @@ public class ArtListControlViewPage extends ArtListControlPage {
 	private View footerView;
 	private LayoutInflater inflater;
 	private ListView listView;
-	private boolean isLastRow = false;
-	private int lastPage =0;
+
 
 	public ArtListControlViewPage(Activity window,ListAdapter adapter,ListView listView){
 		super(window,adapter);
@@ -29,9 +28,9 @@ public class ArtListControlViewPage extends ArtListControlPage {
 		footerView = (View) inflater.inflate(R.layout.artlistview_footer,null);
 		headerView = (View) inflater.inflate(R.layout.artlistview_head,null);
 //		listView.addHeaderView(headerView);
-		listView.addFooterView(footerView);
-		listView.setAdapter(adapter);
-		listView.setOnScrollListener(this);
+		this.listView.addFooterView(footerView);
+		this.listView.setAdapter(this.adapter);
+		this.listView.setOnScrollListener(this);
 
 		arrowImageView = (ImageView) headerView
 				.findViewById(R.id.head_arrowImageView);
@@ -47,24 +46,14 @@ public class ArtListControlViewPage extends ArtListControlPage {
 	}
 
 
-	@Override
-	public void onScroll(AbsListView view, int firstVisibleItem,
-						 int visibleItemCount, int totalItemCount) {
-		if (!isLastRow&&firstVisibleItem + visibleItemCount == totalItemCount && totalItemCount > 0 ) {
-			isLastRow = true;
-		}
-	}
 
 	@Override
-	public void onScrollStateChanged(AbsListView view, int scrollState) {
-		Log.e(tag, "onScrollStateChanged");
-		if (lastPage !=page&&isLastRow && scrollState == AbsListView.OnScrollListener.SCROLL_STATE_IDLE) {
-			//加载元素
-			loadData();
-			isLastRow = false;
-			lastPage =page;
-		}
+	protected void loadData() {
+		super.loadData();
+
 	}
 
-
+	protected void finishLoadData(){
+		this.listView.removeFooterView(footerView);
+	}
 }
